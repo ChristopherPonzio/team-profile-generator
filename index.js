e8e4dae35b2ae2a9e03b0c72cacc04225376f36d
Christopher Ponzio <chrisponz11@yahoo.com>
2022-03-05 16:16:46 -0500

    linked files together

---
+++ index.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const jest = require('jest');
+
+
+// Constructors
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
