c283f40ad8c1e3e55b949964b6c3665b51516946
Christopher Ponzio <chrisponz11@yahoo.com>
2022-03-06 10:33:10 -0500

    test for manager and engineer added

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -1,1 +1,19 @@
 const Engineer = require('../lib/Engineer');
+const Employee = require('../lib/Employee');
+
+describe('Engineer', () => {
+
+    test('engineer extend employee', () => {
+        const engineer = new Engineer('Michelle');
+    });
+    test('set github', () => {
+        const value = GitHubAccount;
+        const engineer = new Engineer('Michelle', 1, 'michelle@email.com', value);
+        expect(engineer.github).toBe(value);
+    });
+    test('set new role', () => {
+        const value = 'Engineer';
+        const engineer = new Engineer('Michelle', 1, 'michelle@email.com', 'mblackGitHub');
+        expect(engineer.getRole()).toBe(value);
+    });
+})
--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -1,1 +1,13 @@
 const Manager = require('../lib/Manager');
+const Employee = require('../lib/Employee');
+
+test('manager office number', () => {
+    const value = 100;
+    const manager = new Manager('Sandy', 1, "sandy@email.com", value);
+    expect(manager.getOfficeNumber()).toBe(value);
+});
+test('set new role', () => {
+    const value = Manager();
+    const manager = new Manager('Sandy', 1, "sandy@email.com", 100);
+    expect(manager.getRole()).toBe(value);
+});
