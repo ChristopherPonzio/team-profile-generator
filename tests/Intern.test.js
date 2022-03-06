607766ec531c49b263e7ec17ea09643e00a4e8e0
Christopher Ponzio <chrisponz11@yahoo.com>
2022-03-06 10:45:50 -0500

    added intern test

--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -1,1 +1,18 @@
 const Intern = require('../lib/Intern');
+const Employee = require('../lib/Employee');
+
+test('set school', () => {
+    const value = 'Yale';
+    const intern = new Intern('Dutch', 1, 'dutch@ElementInternals.com', value);
+    expect(intern.school).toBe(value);
+});
+test('getSchool()', () => {
+    const value = 'Yale';
+    const intern = new Intern('Dutch', 1, 'dutch@ElementInternals.com', value);
+    expect(intern.getSchool()).toBe(value);
+});
+test('set new role', () => {
+    const value = 'Intern';
+    const intern = new Intern('Dutch', 1, 'dutch@ElementInternals.com', 'Yale');
+    expect(intern.getRole()).toBe(value);
+});
