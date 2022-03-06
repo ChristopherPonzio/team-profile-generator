0e32952bea187e12ac5f75a745a36c13850917ec
Christopher Ponzio <chrisponz11@yahoo.com>
2022-03-06 09:40:40 -0500

    added employee test

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,1 +1,23 @@
 const Employee = require('../lib/Employee');
+
+describe('Employee', () => {
+
+    test('create employee', () => {
+        const employee = new Employee('Chris');
+    });
+    test('set id',  () => {
+        const value = 100000;
+        const employee = new Employee('Chris', value);
+        expect(employee.id).toBe(value);
+    });
+    test('set email', () => {
+        const value = 'somebody@email.com';
+        const employee = new Employee('Chris', 1, value);
+        expect(employee.email).toBe(value);
+    });
+    test('set role', () => {
+        const value = 'Employee';
+        const employee = new Employee('Chris', 1, 'somebody@email.com');
+        expect(employee.getRole()).toBe(value);
+    });
+})
